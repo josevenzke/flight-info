0cda31a5688e31f438fb76400b299ba7bba7433e
José Venzke <josevenzkejr@gmail.com>
2021-11-29 18:13:55 -0300

    scrapping data from aisweb

--- flight/info/functions.py
+++ flight/info/functions.py
@@ -4,27 +4,67 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-def scrap_data(name):
+
+def scrap_data(name: str) -> dict:
+    """Coleta informações de aerodromos através do
+    site da AISWEB e as retorna em um dict"""
+
     options = Options()
     options.headless = True
     options.add_argument("--window-size=1920,1080")
 
-    s=Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(options=options,service=s)
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=options, service=s)
     driver.get(f"https://aisweb.decea.mil.br/?i=aerodromos&codigo={name}")
 
-    sunrise = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div[2]/div[1]/div[1]/h4/sunrise')
-    sunset = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div[2]/div[1]/div[2]/h4/sunset')
-    metar = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div[2]/p[2]')
-    tarf = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div[2]/p[3]')
-    
-    div = driver.find_element(By.XPATH,'/html/body/div/div/div/div[2]/div[2]')
-    listas = div.find_elements(By.TAG_NAME, "li a")
+    try:
+        error = driver.find_element(
+            By.XPATH, "/html/body/div/div/section/div/div[1]/div/div"
+        )
+        return False
+    except:
+        pass
 
-    rows_list = []
+    try:
+        sunrise = driver.find_element(
+            By.XPATH, "/html/body/div/div/div/div[2]/div[2]/div[1]/div[1]/h4/sunrise"
+        )
+        sunset = driver.find_element(
+            By.XPATH, "/html/body/div/div/div/div[2]/div[2]/div[1]/div[2]/h4/sunset"
+        )
+    except:
+        sunrise = "Sem info"
+        sunset = "Sem info"
+
+    try:
+        metar = driver.find_element(
+            By.XPATH, "/html/body/div/div/div/div[2]/div[2]/p[2]"
+        )
+        tarf = driver.find_element(
+            By.XPATH, "/html/body/div/div/div/div[2]/div[2]/p[3]"
+        )
+    except:
+        sunrise = "Sem info"
+        sunset = "Sem info"
+
+    try:
+        div = driver.find_element(By.XPATH, "/html/body/div/div/div/div[2]/div[2]")
+        listas = div.find_elements(By.TAG_NAME, "li a")
+    except:
+        listas = []
+
+    links_info = []
     for i in range(len(listas)):
         date = listas[i]
 
-        rows_list.append([date.text,date.get_attribute('href')])
+        links_info.append({"text": date.text, "link": date.get_attribute("href")})
 
-    return rows_list
+    context = {
+        "sunrise": sunrise.text,
+        "sunset": sunset.text,
+        "metar": metar.text,
+        "tarf": tarf.text,
+        "links": links_info,
+    }
+
+    return context
