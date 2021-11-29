f673ce0498ffca9ca73b20df434debabc42f7410
José Venzke <josevenzkejr@gmail.com>
2021-11-29 18:14:09 -0300

    now handling post requests

--- flight/info/views.py
+++ flight/info/views.py
@@ -4,10 +4,15 @@ from .functions import scrap_data
 
 # Create your views here.
 def teste(request):
-    if request.method == 'POST':
-        nome = request.POST.get('nome')
-        valores = scrap_data(nome)
-        context={'valores':valores}
-        return render(request,'index.html',context)
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        info = scrap_data(nome)
+        if info:
+            context = {"info": info}
+        else:
+            context = {
+                "error": f"Não foi possível encontrar informações sobre a base: {nome}"
+            }
+        return render(request, "index.html", context)
 
-    return render(request,'index.html')
+    return render(request, "index.html")
