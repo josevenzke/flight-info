6469aef5b194f48139a6b5e688bf8e8ea37a923c
José Venzke <josevenzkejr@gmail.com>
2021-11-29 19:21:48 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# flight-info
+
+## Manual de Execução
+
+### Clone o repositório:
+`git clone git@github.com:josevenzke/flight-info.git`
+
+### Crie um ambiente virtual da sua forma preferida
+## E certifique-se de ter o ChromeDriver instalado
+
+### Instale os requirements:
+`pip install -r requirements.txt`
+
+### Entre no root do projeto:
+`cd flight/`
+
+### Inicie a aplicação:
+`python manage.py runserver`
--- flight/flight/settings.py
+++ flight/flight/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'zwsjm*pm62y6mr5iak6wf+k#pi%)nqpwa5!ikq#1+1=#_0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',cast=bool)
+DEBUG = True
+
 
 ALLOWED_HOSTS = ["*"]
 
